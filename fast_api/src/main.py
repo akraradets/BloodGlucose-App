c895993e254be65cf09dec0f6297a16f925f7997
akraradets <akraradets@gmail.com>
2025-09-14 14:58:58 +0700

    adding fastapi for web and grpc proxy

---
+++ fast_api/src/main.py
@@ -0,0 +1,35 @@
+
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from src.web import router as web_routers
+
+
+import os 
+_build_version = os.environ.get("BUILD_VERSION", "0.1.0")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # All things needed to do before app is running
+    yield
+    # All things needed to do when shutting off
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    root_path=os.environ.get("FASTAPI_ROOT_PATH", "")
+    )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(web_routers)
+# app.include_router(web_routers, prefix="")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
---
+++ fast_api/src/web/__init__.py
@@ -0,0 +1,28 @@
+from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+router = APIRouter(
+    responses={
+        404: {"description": "Not found (e.g., file path, resource)"},
+        422: {"description": "Unprocessable Entity (e.g., invalid input format/values)"},
+        500: {"description": "Internal Server Error"}
+    },
+)
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_homepage(request: Request) -> HTMLResponse:
+    # return templates.TemplateResponse("homepage.html", {"request": request})
+    return templates.TemplateResponse(name="home.html", context={"request": request})
+
+@router.get("/blood", response_class=HTMLResponse)
+async def get_blood(request: Request) -> HTMLResponse:
+    # return templates.TemplateResponse("homepage.html", {"request": request})
+    return templates.TemplateResponse(name="blood.html", context={"request": request})
+
+# async def get_item(request: Request, id: str) -> HTMLResponse:
+#     return templates.TemplateResponse(
+#         request=request, name="item.html", context={"id": id}
+#     )
