b6e1002fa1142e3432730cce9f2b2c92159e91dc
akraradets <akraradets@gmail.com>
2025-09-16 20:15:16 +0700

    add some pages in html

--- fast_api/src/web/__init__.py
+++ fast_api/src/web/__init__.py
@@ -1,7 +1,7 @@
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, HTTPException, Request
-
+from datetime import datetime
 router = APIRouter(
     responses={
         404: {"description": "Not found (e.g., file path, resource)"},
@@ -15,7 +15,11 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/", response_class=HTMLResponse)
 async def get_homepage(request: Request) -> HTMLResponse:
     # return templates.TemplateResponse("homepage.html", {"request": request})
-    return templates.TemplateResponse(name="home.html", context={"request": request})
+    return templates.TemplateResponse(name="home.html", 
+                                      context={
+                                          "request": request, 
+                                          "date": datetime.now().strftime("%Y-%m-%d"),
+                                          "time": datetime.now().strftime("%H:%M")})
 
 @router.get("/device_control", response_class=HTMLResponse)
 async def get_device_control(request: Request) -> HTMLResponse:
